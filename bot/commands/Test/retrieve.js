749f80ac9c218c2ba1c0f97cacffef0c43338a34
Emily J <me@emily.im>
2020-10-10 16:07:15 +1100

    access all 3 db's instead of just guild

--- bot/commands/Test/retrieve.js
+++ bot/commands/Test/retrieve.js
@@ -10,14 +10,14 @@ class Retrieve extends Command {
   }
 
   async run (message, args, data) { // eslint-disable-line no-unused-vars
-		if (!args[0]) return message.channel.send("You didn't specify what key to retrieve!")
-
-		try {
-			const res = await this.client.db.getGuildKey(message.guild.id, args[0])
-			message.channel.send(res)
-		} catch (err) {
-			return message.channel.send(err)
+		if (!args[0]) return message.channel.send("You didn't specify what database to access!");
+		if (args[0].toLowerCase() !== 'guild' && args[0].toLowerCase() !== 'member' && args[0].toLowerCase() !== 'user') {
+			return message.channel.send('Invalid database. Valid databases: `guild`, `member`, `user`');
 		}
+
+		let res = data[args[0]][args[1]];
+		if (!res) return message.channel.send('Invalid key. Check for typing errors and try again.');
+		message.channel.send('```' + res + '```')
   }
 }
 
