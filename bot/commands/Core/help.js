cfa2f53f56f7d80b8facfe8cff77f82d6fc7f587
Emily J <me@emily.im>
2020-10-10 19:37:11 +1100

    fixed object stuff

--- bot/commands/Core/help.js
+++ bot/commands/Core/help.js
@@ -3,9 +3,9 @@ const Command = require("../../base/Command.js");
 class Help extends Command {
   constructor (client) {
     super(client, {
-      description = "Lists what commands Woomy has, what they do, and how to use them.",
-      usage = "'`help` - Lists all commands.\n`help <command>` - Shows detailed information on a specific command.'",
-      aliases = ["cmds"]
+      description: "Lists what commands Woomy has, what they do, and how to use them.",
+      usage: "'`help` - Lists all commands.\n`help <command>` - Shows detailed information on a specific command.'",
+      aliases: ["cmds"],
     });
   }
 
