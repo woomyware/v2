98f47c3ac7735994cffd551ee707cf56b5ded62a
Emily J <me@emily.im>
2020-10-10 13:46:18 +1100

    tanuki

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -5,7 +5,7 @@ module.exports = class {
 
   async run () {
     await this.client.wait(1000);
-    this.client.user.setActivity('eating the rich') // lol
+    this.client.user.setActivity(`BNA | v${this.client.package.version}`, { type: "WATCHING" }) // lol
     this.client.logger.ready(`Connected to Discord as ${this.client.user.tag}`);
   }
 };
