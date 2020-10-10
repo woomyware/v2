33773b6b37c4aa27d29184c996e17340cf9b44b2
Emily J <me@emily.im>
2020-10-10 14:59:41 +1100

    changes to notes

--- bot/util/dbdoctor.js
+++ bot/util/dbdoctor.js
@@ -1,4 +1,8 @@
 /*
- * Will work on this soon. Triggered when a mismatch between DB and Discord occurs
- * For instance, when a user deletes a role that the database has stored.
+ * - Will work on this soon. Triggered when a mismatch between DB and Discord occurs
+ *   For instance, when a user deletes a role that the database has stored.
+ * 
+ * - Will be implemented so if Woomy fails to find a role, user, etc. with the ID stored in
+ *   the database, database doctor is triggered. Will attempt to resolve the discrepancy, or reset
+ *   to default
  */
