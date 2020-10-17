dad9231a5f8a3d6cbb4bff2be55b89f2558876c4
Emily J <me@emily.im>
2020-10-17 12:16:17 +1100

    access version directly

--- bot/events/ready.js
+++ bot/events/ready.js
@@ -5,7 +5,7 @@ module.exports = class {
 
   async run () {
     await this.client.functions.wait(1000);
-    this.client.user.setActivity(`BNA | v${this.client.package.version}`, { type: "WATCHING" }) // lol
+    this.client.user.setActivity(`BNA | v${this.client.version}`, { type: "WATCHING" }) // lol
     this.client.logger.ready(`Connected to Discord as ${this.client.user.tag}`);
   }
 };
