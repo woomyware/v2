22f51c78d2db4f631846389c741f87c9265335be
Emily J <me@emily.im>
2020-10-21 11:38:55 +1100

    upload command/event templates

---
+++ examples/exampleCommand.js
@@ -0,0 +1,26 @@
+/* eslint-disable */
+
+// Don't modify the name or category variables, they are set automatically by the loaders.
+module.exports = class {
+    constructor (name, category) {
+        this.name = name,
+        this.category = category,
+        this.enabled = true,
+        this.guildOnly = false,
+        this.devOnly = false,
+        this.aliases = [],
+        this.userPerms = [],
+        this.botPerms = [],
+        this.cooldown = 2000,
+        this.help = {
+            description: 'description',
+            usage: 'usage',
+            examples: 'examples'
+        };
+    }
+
+    // Main function that is called by the message handler when the command is executed
+    run (client, message, args, data) {
+        
+    }
+};
---
+++ examples/exampleEvent.js
@@ -0,0 +1,13 @@
+/* eslint-disable */ 
+
+// Don't modify any constructor variables here
+module.exports = class {
+    constructor (wsEvent) {
+        this.wsEvent = wsEvent;
+    }
+
+    // The main function that is called by the event listener when this event is emitted
+    async run (client) {
+
+    }
+};
