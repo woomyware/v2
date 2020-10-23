d4b00b9522e2504efb8ff4ece0ad2ddfc438edd9
Emily J <me@emily.im>
2020-10-23 19:11:14 +1100

    to-do list

---
+++ bot/commands/Configuration/disable.js
@@ -0,0 +1,21 @@
+module.exports = class {
+    constructor (name, category) {
+        this.name = name,
+        this.category = category,
+        this.enabled = true,
+        this.devOnly = false,
+        this.aliases = [],
+        this.userPerms = [],
+        this.botPerms = [],
+        this.cooldown = 2000,
+        this.help = {
+            description: 'description',
+            usage: 'usage',
+            examples: 'examples'
+        };
+    }
+
+    run (client, message, args, data) {
+        
+    }
+};
