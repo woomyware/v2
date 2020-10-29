3dcf14649ac1269093b1b7308fad8dc04927af1a
Emily J <me@emily.im>
2020-10-30 10:24:57 +1100

    SHUT UP

---
+++ bot/commands/Bot/about.js
@@ -0,0 +1,22 @@
+module.exports = class {
+    constructor (name, category) {
+        this.name = name,
+        this.category = category,
+        this.enabled = true,
+        this.devOnly = false,
+        this.aliases = [],
+        this.userPerms = [],
+        this.botPerms = [],
+        this.cooldown = 2000,
+        this.help = {
+            description: '',
+            arguments: '',
+            details: '',
+            examples: ''
+        };
+    }
+
+    run (client, message, args, data) { //eslint-disable-line no-unused-vars
+        
+    }
+};
