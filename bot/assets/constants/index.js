584c85a50da76fb4d0caafcd34564aaa3fc7a358
Emily J <me@emily.im>
2020-10-29 19:20:16 +1100

    forgot to add categories to index file oops

--- bot/assets/constants/index.js
+++ bot/assets/constants/index.js
@@ -2,5 +2,6 @@ module.exports = {
     activities: require('./activities.json'),
     colours: require('./colours.json'),
     emojis: require('./emojis.json'),
+    categories: require('./categories.json'),
     replies: require ('./replies.json')
 };
