128d2854fd6520d79fa9bb2095045f23ab4c2854
Emily J <me@emily.im>
2020-11-10 14:02:57 +1100

    moved things

--- bot/assets/constants/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    activities: require('./activities.json'),
-    colours: require('./colours.json'),
-    emojis: require('./emojis.json'),
-    categories: require('./categories.json'),
-    replies: require('./replies.json'),
-    exitQuotes: require('./exitQuotes.json').quotes
-};
